d823096be2a6af1c68bf3942a90edb1e5a75fd1f
Mike <mostlyfocusedmike@gmail.com>
2018-12-05 15:12:47 -0500

    add majority of examples

--- server.js
+++ server.js
@@ -14,43 +14,97 @@ const start = async () => {
 
     server.views({
         engines: {
-            html: require('handlebars'),
-            pug: {
-               module: require('pug'),
-               /* Engine specific options go here */
-            }
+            /* simplest way to configure an engine */
+            pug:  require('pug'),
+            /* configure the engine to have specific options */
+            html: {
+                module: require('handlebars'),
+                /* Engine specific options go here */
+                layout: true,
+                relativeTo: __dirname,
+                layoutPath: 'lib/templates/layout',
+                /* where your partial files are stored */
+                partialsPath: 'lib/templates/partials',
+            },
         },
+
+        /* these options configure all engines  */
+
+        /* where the path will start from */
         relativeTo: __dirname,
         /* this tells us where our view files are stored */
         path: 'lib/templates',
+        /*
+            CRUCIAL for refreshing page and getting latest changes in dev
+            without having to go and save a server file to restart the server
+            but in prod, you do want it to be set to true to increase perfomance
+        */
+        isCached: process.env.NODE_ENV === 'production',
         /* global context for all views, regardless of engine */
-        context: {siteTitle: 'The Section 5 Notes'},
-        /* Crucial for refreshing page and getting latest changes */
-        isCached: false,
-         /* only for engines that accept helper files, like handlebars */
-        helpersPath: 'lib/helpers',
+        context: {siteTitle: 'the section 5 notes'},
+        /* files given no extension will be defaulted to this str */
         defaultExtension: 'html',
+        /*
+            you can also use layouts info for your whole project out here
+            The issue in this instance is that Pug does uses layouts
+            differently, so if we put this down here, it will break, see the
+            vision docs for more info
+            https://github.com/hapijs/vision/blob/master/API.md#options
+        */
+        // layout: true,
+        // layoutPath: 'lib/templates/layout'
+        // partialsPath: 'lib/templates/partials',
+        /*
+            some engines use helper files, like handlebars
+            We can put this for all engines here, since if an engine doesn't
+            use helpers, it just ignores it
+        */
+        helpersPath: 'lib/helpers',
     });
 
+    /* load routes */
     server.route({
         method: 'GET',
         path: '/',
         handler: function (request, h) {
-            /* our view */
-            return h.view('index.pug', {name: "tom"});
+            /* our defaultExtension at work here */
+            /* view() takes the filepath to the template, context, and options */
+            return h.view('index', {topic: "Views"}, {});
+            /* context and options are optional */
+        }
+    });
+
+    /* instead of h.view(), you can also use the views handler */
+    // server.route({
+    //     method: 'GET',
+    //     path: '/',
+    //     handler:  {
+    //         template: 'index',
+    //         context: {
+    //             topic: "Views",
+    //         },
+    //     }
+    // });
+
+    /* A view that uses helper functions, check the template */
+    server.route({
+        method: 'GET',
+        path: '/helpers',
+        handler: function (request, h) {
+            return h.view('helpers', {topic: "Helpers"});
         }
     });
 
     server.route({
         method: 'GET',
-        path: '/helpers',
+        path: '/nested',
         handler: function (request, h) {
-
-            return h.view('helpers', {name: "tom"});
+            /* if you use a nested structure, just give the file path */
+            /* we aren't using html so we add the pug extension*/
+            return h.view('nested/example.pug', {topic: "Nesting"});
         }
     });
 
-    /* load routes */
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
 }
